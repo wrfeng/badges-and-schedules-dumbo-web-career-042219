693f8673f7244f08968b0152e9106d26be2e1971
wei feng <theweifeng@gmail.com>
2019-04-11 22:01:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  new_array = []
+  
+  array.each {|ele| new_array << badge_maker(name)}
+  
+  new_array
+end
