b382f0ec79382a0213483c3c667a6bda94756d8c
wei feng <theweifeng@gmail.com>
2019-04-11 22:04:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(array)
   
   new_array
 end
+
+def assign_rooms(array)
+  new_array = []
+  
+  array.each_with_index {|name, idx| new_array << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"
+  
+  new_array
+end
